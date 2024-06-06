f451941a5bc61e911cf35edd0de793fad94f55dc
Carlos Baso <110511071+Basso-23@users.noreply.github.com>
2024-06-06 04:07:39 -0500

    añadi lang

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  i18n: {
+    locales: ["es"],
+    defaultLocale: "es",
+  },
 };
 
 export default nextConfig;
